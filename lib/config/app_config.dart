224e8c84636d086bdaceb5d30fa05d11c3915827
Geoff Lavoy <glavoy@pm.me>
2025-11-29 12:33:40 +0300

    Implemented robust versioning and proper display of last backup datetime stamp

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,14 +1,10 @@
-// lib/config/app_config.dart
 /// Application configuration
 ///
 /// This file contains all configurable application-level settings.
-/// Survey-specific settings are now stored in survey manifest files.
-
-import 'dart:io';
 
 class AppConfig {
   // Software version - displayed in the app and saved with each interview
-  static const String softwareVersion = 'GiSTX 0.0.1';
+  static const String softwareVersion = 'GiSTX 0.0.1.alpha';
 
   // Logging configuration
   static const bool enableDebugLogging = true;
@@ -19,18 +15,4 @@ class AppConfig {
 
   // Database folder path
   static const String databaseFolderPath = 'assets/database';
-
-  // TODO: Remove these deprecated database settings after refactoring db_service.dart
-  // These are temporary compatibility shims until db_service is updated to use survey manifests
-
-  /// @deprecated Use survey manifest databaseName instead
-  static String? get customDatabasePath {
-    if (Platform.isWindows) {
-      return 'C:\\gistx\\database\\fake_household_survey.sqlite';
-    }
-    return null; // Use default internal storage for mobile
-  }
-
-  /// @deprecated Use survey manifest databaseName instead
-  static const String databaseFilename = 'fake_household_survey.sqlite';
 }
